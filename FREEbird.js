c9e013bf68b562f5bf4923cb25860297add4880e
BButsers <157750728+BButsers@users.noreply.github.com>
2024-01-26 23:25:22 +0100

    Update FREEbird.js

--- FREEbird.js
+++ FREEbird.js
@@ -150,7 +150,7 @@ function placePipes() {
     pipeArray.push(bottomPipe);
 
     // Increase the distance between consecutive pipes
-    pipeX += 225; // Adjust this value to change the distance between pipes
+    pipeX += 200; // Adjust this value to change the distance between pipes
 }
 
 function moveBird(e){
